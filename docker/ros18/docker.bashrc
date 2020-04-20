0763148375fa47b443cdbf712d66785e75e6e1ae
Marc Toussaint <marc.toussaint@informatik.uni-stuttgart.de>
2020-04-20 15:23:17 +0200

    docker example with PhysX compilation

--- docker/ros18/docker.bashrc
+++ docker/ros18/docker.bashrc
@@ -2,8 +2,8 @@ export PS1='RAI-DOCKER \u@\h:\w> '
 
 export MAKEFLAGS="-j4 -k"
 
-#export ROSDIR=/opt/ros/kinetic
-#source ${ROSDIR}/setup.bash
+export ROSDIR=/opt/ros/kinetic
+source ${ROSDIR}/setup.bash
 
 alias ls="ls -F --color"
 alias ll="ls -lFh --color"
--- docker/ros18/run-docker.sh
+++ docker/ros18/run-docker.sh
@@ -2,15 +2,6 @@
 
 thispath=$(builtin cd "`dirname "${BASH_SOURCE[0]}"`" > /dev/null && pwd)
 
-# #see http://wiki.ros.org/docker/Tutorials/GUI
-# #https://stackoverflow.com/questions/16296753/can-you-run-gui-apps-in-a-docker-container
-
-# XSOCK=/tmp/.X11-unix
-# XAUTH=/tmp/.docker.xauth
-# touch $XAUTH
-# xauth nlist $DISPLAY | sed -e 's/^..../ffff/' | xauth -f $XAUTH # # nmerge -
-# chmod 755 $XAUTH
-
 xhost +local:root
 
 docker run -it \
@@ -21,11 +12,8 @@ docker run -it \
        --volume="$HOME/.ssh:/root/.ssh:ro" \
        --env="DISPLAY" \
        --network host \
+       --publish 11311:11311 \
        --device /dev/input \
        rai-maintenance-full18 /bin/bash -C $1
-
-#       -v $XSOCK:$XSOCK \
-#       -v $XAUTH:$XAUTH \
-#       -e XAUTHORITY=$XAUTH \
 
 xhost -local:root
